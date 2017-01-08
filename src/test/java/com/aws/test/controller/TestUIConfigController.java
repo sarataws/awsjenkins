def644b217900feed5e2f37f4c607399b2903c67
Mallikarjuna <mallikarjunraomahesh@gmail.com>
2017-01-08 22:44:40 +0530

    test class

---
+++ src/test/java/com/aws/test/controller/TestUIConfigController.java
@@ -0,0 +1,15 @@
+package com.aws.test.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestUIConfigController {
+
+	@Test
+	public void testGetUserDetails() throws Exception {
+		System.out.println("This is my test class");
+	}
+
+}
